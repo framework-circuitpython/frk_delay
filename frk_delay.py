42542ee299a98e427d8066fda9002198ef357d5a
Nathan Woody <nathan@nathanwoody.com>
2022-11-10 04:05:12 +0000

    Initial commit
    Added current .py and blank README

---
+++ frk_delay.py
@@ -0,0 +1,28 @@
+from framework import Driver
+import asyncio
+
+class Delay(Driver):
+    _defaults = {'sleep' = 0.0,
+                 'initial_value': False,
+                 'value': False,
+                 'delay': 0.5,
+                 'start': False,
+                 'started': False,
+                 'event': False,
+                 'on_event': []}
+
+    async def _run(self):
+        self._value = self._initital_value
+        while True:
+            if self._started:
+                self._value = not self._value
+                self._handle_event('event')
+                self._started = False
+                _sleep = self._sleep
+            elif self._start and not self._started:
+                self._start = False
+                self._started = True
+                _sleep = self._delay
+            else:
+                _sleep = self._sleep
+            await asyncio.sleep(_sleep)
